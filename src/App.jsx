b26e746579d02daa3435daaada5af5cfc36dff3e
XenoAzure <franco.dog@hotmail.com>
2026-03-29 18:51:18 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import { Route, Routes } from 'react-router'
 import LoginScreen from './Screens/LoginScreen/LoginScreen'
 import RegisterScreen from './Screens/RegisterScreen/RegisterScreen'
 import ResetPasswordRequestScreen from './Screens/ResetPasswordRequestScreen/ResetPasswordRequestScreen'
-import AuthContextProvider from './context/AuthContext'
 import AuthMiddleware from './Middlewares/AuthMiddleware'
 import HomeScreen from './Screens/HomeScreen/HomeScreen'
 
@@ -14,14 +13,14 @@ const App = () => {
       <Route path='/login' element={<LoginScreen />} />
       <Route path='/register' element={<RegisterScreen />} />
       <Route path='/' element={<LoginScreen />} />
-      <Route 
-        path="/reset-password-request" 
-        element={<ResetPasswordRequestScreen/>}
+      <Route
+        path="/reset-password-request"
+        element={<ResetPasswordRequestScreen />}
       />
-      <Route element={<AuthMiddleware/>}>
-        <Route 
-          path='/home' 
-          element={<HomeScreen/>}
+      <Route element={<AuthMiddleware />}>
+        <Route
+          path='/home'
+          element={<HomeScreen />}
         />
       </Route>
     </Routes>
