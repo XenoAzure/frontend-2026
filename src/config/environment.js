7b8790699b32c2d54ae3759f807d95448bfd3843
XenoAzure <franco.dog@hotmail.com>
2026-03-30 15:32:09 -0300

    Update environment config and dependencies

--- src/config/environment.js
+++ src/config/environment.js
@@ -1,5 +1,6 @@
+const apiUrl = import.meta.env.VITE_API_URL || '';
 const ENVIRONMENT = {
-    API_URL: import.meta.env.VITE_API_URL
+    API_URL: apiUrl.endsWith('/') ? apiUrl.slice(0, -1) : apiUrl
 }
 
 export default ENVIRONMENT
